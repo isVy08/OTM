50a8cf250d892bcbb52afd1fd34556695c161097
Vy Vo <vyvo2608@gmail.com>
2023-10-30 14:49:33 +1100

    Initial commit

--- parse.py
+++ parse.py
@@ -1,48 +1,82 @@
 import pandas as pd 
 from utils.io import load_txt
+import numpy as np
 '''
 Columns: Code, OTM, Mean, SK, RR, Iterative, MissDag
 Rows: Fscore, Gscore, SDH
 '''
-baseline = load_txt('output/baseline_linear.txt')
-output = {}
-for line in baseline:
-    if 'ER' in line or 'SF' in line: 
-        code = line.split('-')[:2]
-        code = '-'.join(code)
-        if code not in output:
-            output[code] = {}
-        method = line.split('-')[2:]
-        method = '-'.join(method)
-        if method not in output[code]:
-            output[code][method] = {}
-    elif 'F1' in line or 'gscore' in line or 'shd' in line:
-        v = line.split(' : ')[-1]
-        m = line.split(' : ')[0]
-        output[code][method][m] = v
+
+def extract_baseline(output, sem_type='linear'):
+
+    graph = load_txt(f'output/baseline_{sem_type}.txt')
     
-otm = load_txt('output/otm_linear.txt')
-missdag = load_txt('output/missdag_linear.txt')
 
-for line in otm:
-    if 'ER' in line or 'SF' in line: 
-        curr_code = line
-    elif 'F1' in line or 'gscore' in line or 'shd' in line:
-        value = line.split(' : ')[-1]
-        m = line.split(' : ')[0]
-        if 'otm' not in output[curr_code]:
-            output[curr_code]['otm'] = {}
-        output[curr_code]['otm'][m] = value
+    for line in graph:
+        if 'ER' in line or 'SF' in line: 
+            code = line.split('-')[:2]
+            code = '-'.join(code)
+            if code not in output:
+                output[code] = {}
+            method = line.split('-')[2:]
+            method = '-'.join(method)
+            if method not in output[code]:
+                output[code][method] = {}
+        elif 'F1' in line or 'gscore' in line or 'shd' in line:
+            v = line.split(' : ')[-1]
+            m = line.split(' : ')[0]
+            output[code][method][m] = v
 
-for line in missdag:
-    if 'ER' in line or 'SF' in line: 
-        curr_code = line
-    elif 'F1' in line or 'gscore' in line or 'shd' in line:
-        value = line.split(' : ')[-1]
-        m = line.split(' : ')[0]
-        if 'missdag' not in output[curr_code]:
-            output[curr_code]['missdag'] = {}
-        output[curr_code]['missdag'][m] = value
+    imputation = load_txt('output/baseline_imputation.txt')
+    for line in imputation:
+        if ('ER' in line or 'SF' in line) and sem_type.capitalize() in line: 
+            code = line.split('-')[:2]
+            code = '-'.join(code)
+            method = line.split('-')[2:]
+            method = '-'.join(method)
+        elif 'MAE' in line: 
+            mae = line.split(', ')[0].split(': ')[-1]
+            rmse = line.split(', ')[1].split(': ')[-1]
+            output[code][method]['MAE'] = np.round(float(mae), 4)
+            output[code][method]['RMSE'] = np.round(float(rmse), 4)
+    return output
+
+def extract_otm_missdag(output, method, sem_type):  
+    graph = load_txt(f'output/{method}_linear.txt')
+    # missdag = load_txt('output/missdag_linear.txt')
+
+    for line in graph:
+        if 'ER' in line or 'SF' in line: 
+            code = line
+        elif 'F1' in line or 'gscore' in line or 'shd' in line:
+            value = line.split(' : ')[-1]
+            m = line.split(' : ')[0]
+            if method not in output[code]:
+                output[code][method] = {}
+            output[code][method][m] = value
+    
+    if method == 'otm':
+        imputation = load_txt(f'output/{method}_imputation.txt')
+        for line in imputation:
+            if ('ER' in line or 'SF' in line) and sem_type.capitalize() in line: 
+                code = line
+            elif 'MAE' in line: 
+                mae = line.split(', ')[0].split(': ')[-1]
+                rmse = line.split(', ')[1].split(': ')[-1]
+                output[code][method]['MAE'] = np.round(float(mae), 4)
+                output[code][method]['RMSE'] = np.round(float(rmse), 4)
+    else:
+        for line in graph:
+            if 'ER' in line or 'SF' in line: 
+                code = line
+            else:
+                output[code][method]['MAE'] = 'NA'
+                output[code][method]['RMSE'] = 'NA'
+    return output
+
+output = extract_baseline({}, 'linear')
+output = extract_otm_missdag(output, 'otm', 'linear')
+output = extract_otm_missdag(output, 'missdag', 'linear')
+# print(output)
 
 code = []
 otm = []
@@ -51,12 +85,11 @@ mean = []
 sk = []
 linrr = []
 iterative = []
-indexs = []
-
+metrics = []
 
 for key, value in output.items():
     
-    for m in ('F1', 'gscore', 'shd'):
+    for m in ('F1', 'gscore', 'shd', 'MAE', 'RMSE'):
         code.append(key)
         otm.append(value['otm'][m])
         missdag.append(value['missdag'][m]) 
@@ -64,21 +97,24 @@ for key, value in output.items():
         sk.append(value['sk'][m]) 
         linrr.append(value['lin-rr'][m]) 
         iterative.append(value['iterative'][m]) 
-        indexs.append(m)
+        metrics.append(m)
 
-print(otm)
+df = pd.DataFrame(data={
+    'Metric': metrics,
+    'Code' : code, 
+    'OTM': otm, 
+    'MissDAG': missdag, 
+    'Mean Imputer': mean, 
+    'SK Imputer': sk,
+    'RR Imputer': linrr, 
+    'Iterative Imputer': iterative
+})
 
-# df = pd.DataFrame(data={
-#     'Metric': indexs,
-#     'Code' : code, 
-#     'OTM': otm, 
-#     'MissDAG': missdag, 
-#     'Mean Imputer': mean, 
-#     'SK Imputer': sk,
-#     'RR Imputer': linrr, 
-#     'Iterative Imputer': iterative
-# })
+
+df.to_csv('output/final.csv', index = False)
+
+
+
 
 
 # # print(df.head(100))
-# df.to_csv('output/final.csv', index = False)
