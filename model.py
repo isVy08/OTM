7597fd571e25590bd4416211d75939d71a87810f
Vy Vo <vyvo2608@gmail.com>
2023-12-06 15:38:44 +1100

    Initial commit

--- model.py
+++ model.py
@@ -181,8 +181,10 @@ class DagmaNonlinear:
             
             l1_reg = lambda1 * self.model.scm.fc1_l1_reg()
             
-            if self.model.sem_type == 'mim':
+            if self.model.sem_type == 'mlp':
                 obj = mu * (score + l1_reg) + h_val + 1.5 * rbf_kernel(Xhat, X)
+            elif self.model.sem_type == 'real':
+                obj = mu * (score + l1_reg) + h_val + 0.001 * rbf_kernel(Xhat, X)
             else:
                 obj = mu * (score + l1_reg) + h_val + 0.01 * rbf_kernel(Xhat, X)
 
