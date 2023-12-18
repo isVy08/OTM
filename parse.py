d45ee5c2dab7146b3530c971f268f45b5d2db6d2
Vy Vo <52537680+isVy08@users.noreply.github.com>
2023-12-18 11:16:46 +1100

    Initial commit

--- parse.py
+++ parse.py
@@ -6,9 +6,9 @@ Columns: Code, OTM, Mean, SK, RR, Iterative, MissDag
 Rows: Fscore, Gscore, SDH
 '''
 
-def extract_baseline(output, sem_type='linear'):
+def extract_baseline(output, sem_type, version):
 
-    graph = load_txt(f'output/baseline_{sem_type}.txt')
+    graph = load_txt(f'output/{version}/baseline_{sem_type}.txt')
     
     for line in graph:
         if 'ER' in line or 'SF' in line or 'REAL' in line: 
@@ -25,9 +25,9 @@ def extract_baseline(output, sem_type='linear'):
         elif 'F1' in line or 'tpr' in line or 'shd' in line:
             v = line.split(' : ')[-1]
             m = line.split(' : ')[0]
-            output[code][method][m] = v
+            output[code][method][m] = 0.2 * float(v)
 
-    imputation = load_txt(f'output/baseline_{sem_type}_imputation.txt')
+    imputation = load_txt(f'output/{version}/baseline_{sem_type}_imputation.txt')
    
     sem_type = 'Linear' if sem_type == 'linear' else sem_type.upper()
    
@@ -42,28 +42,32 @@ def extract_baseline(output, sem_type='linear'):
         elif 'MAE' in line: 
             mae = line.split(', ')[0].split(': ')[-1]
             rmse = line.split(', ')[1].split(': ')[-1]
-            output[code][method]['MAE'] = np.round(float(mae), 4)
-            output[code][method]['RMSE'] = np.round(float(rmse), 4)
+            output[code][method]['MAE'] = 0.2 * np.round(float(mae), 4)
+            output[code][method]['RMSE'] = 0.2 * np.round(float(rmse), 4)
     return output
 
-def extract_otm_missdag(output, method, sem_type):  
-    graph = load_txt(f'output/{method}_{sem_type}.txt')
+def extract_otm_missdag(output, method, sem_type, version):  
+    graph = load_txt(f'output/{version}/{method}_{sem_type}.txt')
 
 
     for line in graph:
+
         if 'ER' in line or 'SF' in line or 'REAL' in line: 
             if 'GP-ADD' in line: 
                 line = line.replace('GP-ADD', 'GPADD')
             code = line
+            if code not in output:
+                output[code] = {}
+
         elif 'F1' in line or 'tpr' in line or 'shd' in line:
             value = line.split(' : ')[-1]
             m = line.split(' : ')[0]
             if method not in output[code]:
                 output[code][method] = {}
-            output[code][method][m] = value
+            output[code][method][m] = 0.2 * float(value)
     
     if method == 'otm':
-        imputation = load_txt(f'output/otm_{sem_type}_imputation.txt')
+        imputation = load_txt(f'output/{version}/otm_{sem_type}_imputation.txt')
         sem_type = 'Linear' if sem_type == 'linear' else sem_type.upper()
         for line in imputation:
             if ('ER' in line or 'SF' in line or 'REAL' in line) and sem_type in line: 
@@ -73,8 +77,8 @@ def extract_otm_missdag(output, method, sem_type):
             elif 'MAE' in line: 
                 mae = line.split(', ')[0].split(': ')[-1]
                 rmse = line.split(', ')[1].split(': ')[-1]
-                output[code][method]['MAE'] = np.round(float(mae), 4)
-                output[code][method]['RMSE'] = np.round(float(rmse), 4)
+                output[code][method]['MAE'] = 0.2 * np.round(float(mae), 4)
+                output[code][method]['RMSE'] = 0.2 * np.round(float(rmse), 4)
     else:
         for line in graph:
             if 'ER' in line or 'SF' in line or 'REAL' in line:
@@ -82,56 +86,73 @@ def extract_otm_missdag(output, method, sem_type):
                     line = line.replace('GP-ADD', 'GPADD')
                 code = line
             else:
-                output[code][method]['MAE'] = 'NA'
-                output[code][method]['RMSE'] = 'NA'
+                output[code][method]['MAE'] = 0
+                output[code][method]['RMSE'] = 0
     return output
 
-sem_type = 'real'
-output = extract_baseline({}, sem_type) 
-output = extract_otm_missdag(output, 'otm', sem_type)
-# output = extract_otm_missdag(output, 'complete', sem_type)
-# output = extract_otm_missdag(output, 'missdag', sem_type)
-# print(output)
+def collect(method, sem_type): 
 
-code = []
-otm = []
-missdag = []
-mean = []
-sk = []
-linrr = []
-iterative = []
-# complete = []
-metrics = []
+    for i in range(1, 6):
+        version = f'v{i}'
+        if i == 1:
+            if method == 'baseline':
+                output = extract_baseline({}, sem_type, version)
+            else:
+                output = extract_otm_missdag({}, method, sem_type, version)
+        else: 
+            if method == 'baseline':
+                temp = extract_baseline({}, sem_type, version)
+            else:
+                temp = extract_otm_missdag({}, method, sem_type, version)
+            # levels: code > method > metric = value
+            for code, l1_val in temp.items():
+                for method, l2_val in l1_val.items():  
+                    for metric, value in l2_val.items():
+                        output[code][method][metric] += value  
+    return output
 
-output = dict(sorted(output.items()))
-for key, value in output.items():
-    
-    for m in ('F1', 'tpr', 'shd', 'MAE', 'RMSE'):
-        code.append(key)
-        otm.append(value['otm'][m])
+def output_to_df(output, sem_type):
+    code = []
+    otm = []
+    missdag = []
+    mean = []
+    sk = []
+    linrr = []
+    iterative = []
+    complete = []
+    metrics = []
 
-        # if m in ('MAE', 'RMSE'):
-        #     complete.append(0)
-        # else:
-        #     complete.append(value['complete'][m])
-        # missdag.append(value['missdag'][m]) 
-        mean.append(value['mean'][m]) 
-        sk.append(value['sk'][m]) 
-        linrr.append(value['lin-rr'][m]) 
-        iterative.append(value['iterative'][m]) 
-        metrics.append(m)
+    output = dict(sorted(output.items()))
+    for key, value in output.items():
+        
+        for m in ('F1', 'tpr', 'shd', 'MAE', 'RMSE'):
+            code.append(key)
+            otm.append(value['otm'][m])
 
-df = pd.DataFrame(data={
-    'Metric': metrics,
-    'Code' : code, 
-    'OTM': otm, 
-    # 'MissDAG': missdag, 
-    'Mean Imputer': mean, 
-    'SK Imputer': sk,
-    'RR Imputer': linrr, 
-    'Iterative Imputer': iterative,
-    # 'Complete': complete,
-})
+            if m in ('MAE', 'RMSE'):
+                complete.append(0)
+            else:
+                complete.append(value['complete'][m])
+            missdag.append(value['missdag'][m]) 
+            mean.append(value['mean'][m]) 
+            sk.append(value['sk'][m]) 
+            linrr.append(value['lin-rr'][m]) 
+            iterative.append(value['iterative'][m]) 
+            metrics.append(m)
 
-df.to_csv('output/final.csv', index = False)
+    df = pd.DataFrame(data={
+        'Metric': metrics,
+        'Code' : code, 
+        'OTM': otm, 
+        'MissDAG': missdag, 
+        'Mean Imputer': mean, 
+        'SK Imputer': sk,
+        'RR Imputer': linrr, 
+        'Iterative Imputer': iterative,
+        'Complete': complete,
+    })
 
+    df.to_csv(f'output/{sem_type}.csv', index = False)
+
+
+output_otm = collect('otm', 'mlp')
